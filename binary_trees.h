98e86c99708a5542fbcdd9b4baf74f337488de7e
habibaadam <habiepalmer@gmail.com>
2023-11-02 08:41:47 +0000

    fixing 121-avl-insert

--- binary_trees.h
+++ binary_trees.h
@@ -91,5 +91,8 @@ bst_t *bst_delete(bst_t *root, bst_t *node);
 bst_t *bst_remove_recursive(bst_t *root, bst_t *node, int value);
 int is_avl_helper(const binary_tree_t *tree, int lo, int hi);
 size_t height(const binary_tree_t *tree);
+int balance(const binary_tree_t *tree);
+avl_t *avl_insert_recursive(avl_t **tree, avl_t *parent,
+avl_t **new, int value);
 
 #endif /*BINARY_TREES*/
